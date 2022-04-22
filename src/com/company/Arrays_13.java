963590058ffee30ca88ba9d5be774afe88a2c97e
Vaishali <vaishalidivate0509@gmail.com>
2022-04-22 18:05:48 +0530

    Java Basic Programs

--- src/com/company/Arrays_13.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-/** Write a method to find the second largest number in an array */
-
-public class Arrays_13 {
-    public static void main(String[] args) {
-
-    }
-}
--- src/com/company/JavaBasic_2.java
+++
@@ -1,9 +0,0 @@
-package com.company;
-
-/** Write a program to print your name */
-
-public class JavaBasic_2 {
-    public static void main(String[] args) {
-        System.out.println("Vaishali Vaijanath Divate");
-    }
-}
